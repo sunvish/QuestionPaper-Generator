f31c20647c353ed61a9ec13809228ab9e0576d23
SUNVISH BHATT <suvishmunna@gmail.com>
2024-07-16 20:00:09 +0530

    installed basic dependencies like express, dotenv, cors and connected to database with mongoose

--- server/index.js
+++ server/index.js
@@ -1,1 +1,21 @@
-import express from "express"
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+// mongodb connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(
+    app.listen(process.env.PORT, () =>
+      console.log(`connected to port ${process.env.PORT}`)
+    )
+  )
+  .catch((error) => console.log(error.message));
