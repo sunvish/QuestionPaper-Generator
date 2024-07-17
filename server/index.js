b602d8fbb1abb17b7d1ee148a340b3ee4283f401
SUNVISH BHATT <suvishmunna@gmail.com>
2024-07-17 16:48:13 +0530

    test

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(
     app.listen(process.env.PORT, () =>
-      console.log(`connected to port ${process.env.PORT}`)
+      console.log(`connected to port number ${process.env.PORT}`)
     )
   )
   .catch((error) => console.log(error.message));
